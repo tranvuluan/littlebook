07b78b01d2f58563feecb65301445f71b5af1420
CuliDev <tranvuluan069@gmail.com>
2022-05-08 23:54:10 +0700

    .

--- config/config.php
+++ config/config.php
@@ -1,6 +1,6 @@
 <?php
-define("DB_HOST", "remotemysql.com");
-define("DB_USER", "TjhdqIdohO");
-define("DB_PASS", "DMSYxKo6F5");
-define("DB_NAME", "TjhdqIdohO");
+define("DB_HOST", "14.225.192.186");
+define("DB_USER", "user1");
+define("DB_PASS", "user1");
+define("DB_NAME", "littlebook");
 ?>
